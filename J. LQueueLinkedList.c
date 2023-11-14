a72472d3ddbf1816d32828388393f9090b94f701
Gigabyte <120817536+Giga4byte@users.noreply.github.com>
2023-11-14 20:17:36 +0530

    Update J. LQueueLinkedList.c

--- J. LQueueLinkedList.c
+++ J. LQueueLinkedList.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int item;
-
 struct node {
     int data;
     struct node *next;
@@ -10,48 +8,54 @@ struct node {
 
 struct node *p, *front, *rear, *ptr;
 
-void enqueue ()
-{
+void enqueue() {
     p = (struct node *)malloc(sizeof(struct node));
     printf("Enter the data: ");
     scanf("%d", &p->data);
-    if (front == NULL && rear == NULL)
-    {   front = p;
+    if (front == NULL && rear == NULL) {
+        front = p;
         rear = p;
-        printf ("Element entered\n");   }
-    else
-    {   rear->next = p;
+        printf("Element entered\n");
+    } else {
+        rear->next = p;
         rear = p;
-        printf ("Element entered\n");   }
+        printf("Element entered\n");
+    }
 }
 
-void dequeue ()
-{
-    if (front == NULL)
-    {   printf ("Queue is empty\n");  }
-    else if (front == rear)
-    {   front = NULL;
+void dequeue() {
+    if (front == NULL) {
+        printf("Queue is empty, cannot dequeue\n");
+    } else if (front == rear) {
+        front = NULL;
         rear = NULL;
-        printf ("Element deleted\n");   }
-    else
-    {   ptr = front;
+        printf("Element deleted\n");
+    } else {
+        ptr = front;
         front = ptr->next;
-        free (ptr);
-        printf ("Element deleted\n");   }
+        free(ptr);
+        printf("Element deleted\n");
+    }
 }
 
-void display ()
-{
+void display() {
     ptr = front;
-    while (ptr!=NULL)
-    {   printf ("%d\t", ptr->data);
-        ptr = ptr->next;    }
+    if (front == NULL) {
+        printf("Queue is empty\n");
+    } else {
+        while (ptr != NULL) {
+            printf("%d\t", ptr->data);
+            ptr = ptr->next;
+        }
+        printf("\n");
+    }
 }
 
 int main() {
     int choice = 0;
-    front = NULL; rear = NULL;
-    printf ("NAME: GIRIBALA\tROLL NO:28\n");
+    front = NULL;
+    rear = NULL;
+    printf("NAME: GIRIBALA\tROLL NO:28\n");
     printf("MENU\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
 
     while (1) {
@@ -59,17 +63,20 @@ int main() {
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1:
-                enqueue(); break;
-            case 2:
-                dequeue(); break;
-            case 3:
-                display(); break;
-            case 4:
-                printf("TERMINATED\n");
-                return 0;
-            default:
-                printf ("INVALID CHOICE, TRY AGAIN\n");
+        case 1:
+            enqueue();
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            printf("TERMINATED\n");
+            return 0;
+        default:
+            printf("INVALID CHOICE, TRY AGAIN\n");
         }
     }
     return 0;
