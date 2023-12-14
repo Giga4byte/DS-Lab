ee08d1f8138c034c8450963a34028f37822ac0f2
Gigabyte <120817536+Giga4byte@users.noreply.github.com>
2023-12-14 20:38:09 +0530

    Update H. LinkedList.c

--- H. LinkedList.c
+++ H. LinkedList.c
@@ -1,74 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int item;
-
 struct node {
     int data;
     struct node *next;
 };
 
-struct node *p, *top, *pointer;
+struct node *p, *front, *rear, *ptr;
 
-void push ()
-{
+void enqueue() {
     p = (struct node *)malloc(sizeof(struct node));
     printf("Enter the data: ");
     scanf("%d", &p->data);
-    if (top == NULL)
-    {   p->next = NULL;
-        top = p;
-        printf ("Element entered\n");   }
-    else
-    {   p->next = top;
-        top = p;    
-        printf ("Element entered\n");   }
+    if (front == NULL && rear == NULL) {
+        front = p;
+        rear = p;
+        printf("Element entered\n");
+    } else {
+        rear->next = p;
+        rear = p;
+        printf("Element entered\n");
+    }
 }
 
-void pop ()
-{
-    if (top == NULL)
-    {   printf ("Stack is empty\n");  }
-    else
-    {   pointer = top;
-        top = top->next;
-        free (pointer);
-        printf ("Element deleted\n");   }
+void dequeue() {
+    if (front == NULL) {
+        printf("Queue is empty, cannot dequeue\n");
+    } else if (front == rear) {
+        front = NULL;
+        rear = NULL;
+        printf("Element deleted\n");
+    } else {
+        ptr = front;
+        front = ptr->next;
+        free(ptr);
+        printf("Element deleted\n");
+    }
 }
 
-void display () 
-{
-    pointer = top;
-    if (top == NULL)
-    {   printf ("Stack is empty\n");  }
-    else
-    {   while (pointer!=NULL)
-        {   printf ("%d\t", pointer->data);
-            pointer = pointer->next;    }    }
+void display() {
+    ptr = front;
+    if (front == NULL) {
+        printf("Queue is empty\n");
+    } else {
+        while (ptr != NULL) {
+            printf("%d\t", ptr->data);
+            ptr = ptr->next;
+        }
+        printf("\n");
+    }
 }
 
 int main() {
     int choice = 0;
-    top = NULL;
-    printf ("NAME: GIRIBALA\tROLL NO:28\n");
-    printf("MENU\n1. Push\n2. Pop\n3. Display\n4. Exit\n");
+    front = NULL;
+    rear = NULL;
+    printf("NAME: GIRIBALA\tROLL NO:28\n");
+    printf("MENU\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
 
     while (1) {
         printf("\nEnter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1:
-                push(); break;
-            case 2:
-                pop(); break;
-            case 3:
-                display(); break;
-            case 4:
-                printf("TERMINATED\n");
-                return 0;
-            default:
-                printf ("INVALID CHOICE, TRY AGAIN\n");
+        case 1:
+            enqueue();
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            printf("TERMINATED\n");
+            return 0;
+        default:
+            printf("INVALID CHOICE, TRY AGAIN\n");
         }
     }
     return 0;
