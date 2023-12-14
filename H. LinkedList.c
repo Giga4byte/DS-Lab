f018e17f2efbd6b198f9c790a5f1db9f1f22e83b
Gigabyte <120817536+Giga4byte@users.noreply.github.com>
2023-12-14 20:37:02 +0530

    Update H. LinkedList.c

--- H. LinkedList.c
+++ H. LinkedList.c
@@ -1,100 +1,160 @@
-// THIS IS INCOMPLETE. DON'T BOTHER COPY-PASTING.
-
 #include <stdio.h>
+#include <stdlib.h>
+
 int item;
 
 struct node {
-    int data;
-    struct node *next;
+	int data;
+	struct node *next;
 };
 
-struct node *p, *head, *tail, *pointer;
+struct node *p, *head, *tail, *pointer, *previous;
 
-void insertAtEnd() {
-    p = (struct node *)malloc(sizeof(struct node));
-    printf("Enter the data: ");
-    scanf("%d", &p->data);
-    p->next = NULL;
-    if (head == NULL) {
-        head = p;
-        tail = p;
-    } else {
-        tail->next = p;
-        tail = p;
-    }
+void insertAtEnd ()
+{
+	p = (struct node*)malloc(sizeof(struct node));
+	printf ("Enter the element to be inserted: ");
+	scanf ("%d", &p->data);
+	p->next = NULL;
+	if (head == NULL)
+	{
+		head = p;
+		tail = p;
+	}
+	else
+	{
+		tail->next = p;
+		tail = p;
+	}
 }
 
-void insertAtBeginning() {
-    p = (struct node *)malloc(sizeof(struct node));
-    printf("Enter the data: ");
-    scanf("%d", &p->data);
-    p->next = NULL;
-    if (head == NULL) {
-        head = p;
-        tail = p;
-    } else {
-        p->next = head;
-        head = p;
-    }
+void insertAtBeginning () 
+{
+	p = (struct node*)malloc(sizeof(struct node));
+	printf ("Enter the element to be inserted: ");
+	scanf ("%d", &p->data);
+	p->next = NULL;
+	if (head == NULL)
+	{
+		head =p;
+		tail = p;
+	}
+	else
+	{
+		p->next = head;
+		head = p;
+	}
 }
 
-void deleteFromBeginning() {
-    if (head == NULL) {
-        printf("The linked list is empty\n");
-    } else {
-        pointer = head;
-        head = pointer->next;
-        free(pointer);
-    }
+void insertAfterNode ()
+{
+	p = (struct node*)malloc(sizeof(struct node));
+	printf ("Enter the element to be inserted: ");
+	scanf ("%d", &p->data);
+	p->next = NULL;
+	pointer = head;
+	printf ("Enter the node: ");
+	scanf ("%d", &item);
+	while (pointer->data != item)
+	{
+		pointer  = pointer->next;
+	}
+	p->next=pointer->next;
+	pointer->next=p;
 }
 
-void display() {
-    pointer = head;
-    printf("GIRIBALA'S LINKED LIST IS\n");
-    while (pointer != NULL) {
-        printf("%d ", pointer->data);
-        pointer = pointer->next;
-    }
-    if (pointer == NULL) {
-        return 0;
-    }
+void deleteFromBeginning ()
+{
+	if (head == NULL)
+	{ printf ("LIST IS EMPTY\n"); }
+	else
+	{
+		pointer = head;
+		head = pointer->next;
+		free (pointer);
+	}
 }
 
-int main() {
-    int choice = 0;
-    printf ("NAME: GIRIBALA\tROLL NO:28\n");
-    head = NULL;
-    printf("MENU\n1. Insert at the end\n2. Insert at the beginning\n3. Insert after a specified node\n4. Delete from the end\n5. Delete from the beginning\n6. Delete a specified node\n7. Display\n8. EXIT\n\n");
+void deleteFromEnd ()
+{
+	if (head == NULL)
+	{ printf ("LIST IS EMPTY"); }
+	else
+	{
+		pointer = head;
+		while (pointer->next != NULL)
+		{
+			previous = pointer;
+			pointer = pointer->next;
+		}
+		previous->next = NULL;
+		tail = previous;
+		free (pointer);
+	}
+}
 
-    while (1) {
-        printf("\nEnter your choice: ");
-        scanf("%d", &choice);
+void deleteNode ()
+{
+	printf ("Enter the node: ");
+	scanf ("%d", &item);
+	pointer = head;
+	//previous = head;
+	if (head == NULL) {
+	printf ("Empty list.\n"); }
+	else {
+	while (pointer->data != item);
+	{
+		previous = pointer;
+		pointer = pointer->next;
+	}
+	previous->next = pointer->next;
+	free (pointer); }
+}
 
-        switch (choice) {
-            case 1:
-                insertAtEnd();
-                break;
-            case 2:
-                insertAtBeginning();
-                break;
-            case 3:
-                insertAfterNode();
-                break;
-            case 4:
-                deleteFromEnd();
-                break;
-            case 5:
-                deleteFromBeginning();
-                break;
-            case 6:
-                deleteAfterNode();
-                break;
-            case 7:
-                display();
-                break;
-            case 8:
-                printf("TERMINATED\n");
-                return 0;
-        }
-    }
+void display ()
+{
+	if (head == NULL)
+	{ printf ("LIST IS EMPTY\n"); }
+	else
+	{
+		pointer = head;
+		while (pointer->next != NULL)
+		{
+			printf ("%d\t", pointer->data);
+			pointer = pointer->next;
+		}
+		printf ("%d\t", pointer->data);
+	}
+}
+
+int main ()
+{
+	int choice = -1; head = NULL;
+	printf ("MENU\n1. End Insert\n2. Beginning Insert\n3. Node Insert\n4. End Delete\n5. Beginning Delete\n6. Node Delete\n7. Display\n8. Exit\n");
+	while (1)
+	{
+		printf ("\nEnter choice: ");
+		scanf ("%d", &choice);
+		switch (choice)
+		{
+			case 1: insertAtEnd();
+					break;
+			case 2: insertAtBeginning();
+					break;
+			case 3: insertAfterNode();
+					break;
+			case 4: deleteFromBeginning();
+					break;
+			case 5: deleteFromEnd();
+					break;
+			case 6: deleteNode();
+					break;
+			case 7: display();
+					break;
+			case 8: printf ("TERMINATED\n");
+					return 0;
+			default: printf ("INVALID CHOICE\n");
+		}
+	}
+	return 0;
 }
