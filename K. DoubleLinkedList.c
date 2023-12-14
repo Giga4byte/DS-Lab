73d46afa85188e7c9373c34284f207bbf9052cd0
Gigabyte <120817536+Giga4byte@users.noreply.github.com>
2023-12-14 20:40:28 +0530

    Update K. DoubleLinkedList.c

--- K. DoubleLinkedList.c
+++ K. DoubleLinkedList.c
@@ -6,7 +6,7 @@ struct node {
     struct node *next, *prev;
 };
 
-struct node *p, *head, *tail;
+struct node *p, *head, *tail, *ptr;
 
 void InsertAtBeginning() {
     p = (struct node *)malloc(sizeof(struct node));
@@ -46,6 +46,69 @@ void InsertAtEnd() {
     }
 }
 
+void InsertAfterNode() {
+    p = (struct node *)malloc(sizeof(struct node));
+    int item;
+    printf("Enter the data: ");
+    scanf("%d", &p->data);
+    p->prev = NULL;
+    p->next = NULL;
+    if (head == NULL) {
+        printf("No linked list exists.\n");
+    } else {
+        printf("Enter the item number: ");
+        scanf("%d", &item);
+        ptr = head;
+        while ((ptr != NULL) && (ptr->data != item)) {
+            ptr = ptr->next;
+        }
+        if (ptr != NULL) {
+            p->next = ptr->next;
+            if (ptr->next != NULL) {
+                ptr->next->prev = p;
+            }
+            p->prev = ptr;
+            ptr->next = p;
+            printf("Element has been inserted.\n");
+        } else {
+            printf("No such element found.\n");
+        }
+    }
+}
+
+void InsertBeforeNode() {
+    p = (struct node *)malloc(sizeof(struct node));
+    int item;
+    printf("Enter the data: ");
+    scanf("%d", &p->data);
+    p->prev = NULL;
+    p->next = NULL;
+    if (head == NULL) {
+        printf("No linked list exists.\n");
+    } else {
+        printf("Enter the item number: ");
+        scanf("%d", &item);
+        ptr = head;
+        while ((ptr != NULL) && (ptr->data != item)) {
+            ptr = ptr->next;
+        }
+        if (ptr != NULL) {
+            if (ptr->prev != NULL) {
+                ptr->prev->next = p;
+            }
+            p->prev = ptr->prev;
+            p->next = ptr;
+            ptr->prev = p;
+            if (ptr == head) {
+                head = p;
+            }
+            printf("Element has been inserted.\n");
+        } else {
+            printf("No such element found.\n");
+        }
+    }
+}
+
 void DeleteFromBeginning() {
     if (head == NULL) {
         printf("List is empty.\n");
@@ -74,6 +137,38 @@ void DeleteFromEnd() {
     }
 }
 
+void DeleteNode() {
+    int item;
+    if (head == NULL) {
+        printf("List is empty.\n");
+    } else {
+        printf("Enter the item number: ");
+        scanf("%d", &item);
+        ptr = head;
+        while ((ptr != NULL) && (ptr->data != item)) {
+            ptr = ptr->next;
+        }
+        if (ptr != NULL) {
+            if (ptr->prev != NULL) {
+                ptr->prev->next = ptr->next;
+            }
+            if (ptr->next != NULL) {
+                ptr->next->prev = ptr->prev;
+            }
+            if (ptr == head) {
+                head = ptr->next;
+            }
+            if (ptr == tail) {
+                tail = ptr->prev;
+            }
+            free(ptr);
+            printf("Element has been deleted.\n");
+        } else {
+            printf("No such element found.\n");
+        }
+    }
+}
+
 void display() {
     printf("DISPLAYING DLL:\n");
     ptr = head;
@@ -86,7 +181,9 @@ void display() {
 
 int main() {
     int choice = 0;
-    printf("MENU:\n1. Insert at the beginning\n2. Insert at the end\n5. Delete from the beginning\n6. Delete from the end\n7. Delete a specified node\n8. Display\n9. Exit\n");
+    head = NULL;
+    tail = NULL;
+    printf("MENU:\n1. Insert at the beginning\n2. Insert at the end\n3. Insert after a specified node\n4. Insert before a specified node\n5. Delete from the beginning\n6. Delete from the end\n7. Delete a specified node\n8. Display\n9. Exit\n");
     while (1) {
         printf("\nEnter your choice: ");
         scanf("%d", &choice);
@@ -104,12 +201,23 @@ int main() {
             case 4:
                 InsertBeforeNode();
                 break;
+            case 5:
+                DeleteFromBeginning();
+                break;
+            case 6:
+                DeleteFromEnd();
+                break;
+            case 7:
+                DeleteNode();
+                break;
             case 8:
                 display();
                 break;
             case 9:
                 printf("TERMINATING.\n");
                 return 0;
+            default:
+            	printf ("INVALID\n");
         }
     }
     return 0;
